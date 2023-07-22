7cf799f617f2f7564500d71c24b9474b3216c88f
TomMahe <mahettom@gmail.com>
2023-07-22 18:48:53 +0200

    import Image component from next/image to optimize image using the provided props | add the blur effect when loading | add function to combineListOfClassname

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,41 @@
-// import Image from 'next/image'
+import { useState } from 'react';
+import Image from 'next/image';
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Gallery() {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <Image />
+        <BlurImage />
 
       </div>
     </div>
   );
 }
 
+function BlurImage() {
 
-function Image() {
+  const [isLoading, setIsLoading] = useState(true)
+
+
   return (
     <a href="#" className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-        <img
+        <Image
           alt=""
           src="https://bit.ly/placeholder-img"
-          className="group-hover:opacity-75"
+          layout="fill"
+          objectFit="cover"
+          onLoadingComplete={() => setIsLoading(false)}
+          className={cn(
+            'group-hover:opacity-75 duration-700 ease-in-out',
+            isLoading
+              ? 'grayscale blur-2xl scale-110'
+              : 'grayscale-0 blur-0 scale-100'
+          )}
         />
       </div>
       <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>

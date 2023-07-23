a0bd638bca3da75dc299b48716ed9ae9de04e0d5
TomMahe <mahettom@gmail.com>
2023-07-23 14:30:45 +0200

    add comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,30 @@ import { createClient } from '@supabase/supabase-js';
 import { useState } from 'react';
 import Image from 'next/image';
 
+
 export async function getStaticProps() {
+  // ——————————————————————————————————————————————————— Create connection to supabase
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.SUPABASE_SERVICE_ROLE_KEY || ''
   )
-
+  // ——————————————————————————————————————————————————— Use supabase & store inside data
+  // ——————————————————————————————————————————————————— Everything from imagesTable
   const { data } = await supabaseAdmin
     .from('imagesTable')
     .select('*')
     .order('id')
-
+  // ——————————————————————————————————————————————————— getStaticProps return data
+  // ——————————————————————————————————————————————————— Inside of the 'images' props
   return {
     props: {
-      imagesTable: data
+      images: data
     }
   }
 }
 
 function combineListOfClassname(...classes: string[]) {
+  // ——————————————————————————————————————————————————— Helper function already provide
   return classes.filter(Boolean).join(' ')
 }
 
@@ -31,10 +36,14 @@ type Image = {
   imgSrc: string
 }
 
+
+// ——————————————————————————————————————————————————— Obtain data from images (getStaticProp's props)
 export default function Gallery({ images }: { images: Image[] }) {
+
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+
         {images.map((image) => (
           <BlurImage key={image.id} image={image} />
         ))}
@@ -52,6 +61,7 @@ function BlurImage({ image }: { image: Image }) {
   return (
     <a href="#" className="group">
       <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+
         <Image
           alt=""
           src="https://bit.ly/placeholder-img"
@@ -65,9 +75,16 @@ function BlurImage({ image }: { image: Image }) {
               : 'grayscale-0 blur-0 scale-100'
           )}
         />
+
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+
+      <h3 className="mt-4 text-sm text-gray-700">
+        Lee Robinson
+      </h3>
+
+      <p className="mt-1 text-lg font-medium text-gray-900">
+        @leeerob
+      </p>
     </a>
   );
 }

5f33752e52d3fdfeb184d1e466a2f16acf22532b
TomMahe <mahettom@gmail.com>
2023-07-23 15:03:30 +0200

    add unsplash domain (hostname)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['bit.ly'],
+        domains: ['images.unsplash.com'],
     },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -83,12 +83,8 @@ function BlurImage({ image }: { image: Image }) {
       </div>
 
       <h3 className="mt-4 text-sm text-gray-700">
-        Lee Robinson
+        @{image.username}
       </h3>
-
-      <p className="mt-1 text-lg font-medium text-gray-900">
-        @leeerob
-      </p>
     </a>
   );
 }
